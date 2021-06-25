79efeb4b2c38cdc765e730bb9205a01c079b4c19
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-25 21:39:49 +0600

    DialogCloseListener interface added

--- app/src/main/java/com/prantomondal/todoandroidapp/AddNewTask.java
+++ app/src/main/java/com/prantomondal/todoandroidapp/AddNewTask.java
@@ -1,6 +1,12 @@
 package com.prantomondal.todoandroidapp;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +14,10 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.core.content.ContextCompat;
+
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.prantomondal.todoandroidapp.Model.ToDoModel;
 import com.prantomondal.todoandroidapp.Utils.DatabaseHandler;
 
 public class AddNewTask extends BottomSheetDialogFragment {
@@ -41,6 +50,68 @@ public class AddNewTask extends BottomSheetDialogFragment {
         db = new DatabaseHandler(getActivity());
         db.openDatabase();
 
+        boolean isUpdate = false;
+        final Bundle bundle = getArguments();
+
+        if(bundle!=null){
+            isUpdate = true;
+            String task = bundle.getString("task");
+            newTaskText.setText(task);
+            if (task.length()>0){
+                newTaskSaveButton.setTextColor(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));
+            }
+            newTaskText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (s.toString().equals("")){
+                        newTaskSaveButton.setEnabled(false);
+                        newTaskSaveButton.setTextColor(Color.GRAY);
+                    }
+                    else{
+                        newTaskSaveButton.setEnabled(true);
+                        newTaskSaveButton.setTextColor(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));;
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+        }
+        boolean finalIsUpdate = isUpdate;
+        newTaskSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = newTaskText.getText().toString();
+                if(finalIsUpdate){
+                    db.updateTask(bundle.getInt("id"),text);
+                }
+                else{
+                    ToDoModel task = new ToDoModel();
+                    task.setTask(text);
+                    task.setStatus(0);
+                }
+                dismiss();
+
+            }
+        });
+
+
     }
+    @Override
+    public void onDismiss(DialogInterface dialog){
+        Activity activity = getActivity();
+        if (activity instanceof DialogCloseListener){
+            ((DialogCloseListener)activity).handleDialogClose(dialog);
+        }
+    }
+
+
 
 }
---
+++ app/src/main/java/com/prantomondal/todoandroidapp/DialogCloseListener.java
@@ -0,0 +1,4 @@
+package com.prantomondal.todoandroidapp;
+
+public interface DialogCloseListener {
+}
